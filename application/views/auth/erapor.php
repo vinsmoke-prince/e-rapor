3ed77bfe9a0a9f2ed2de4d83b2c2fedab85f60bb
vinsmoke-prince <alipfajar24@gmail.com>
2022-05-15 12:54:18 +0700

    manambahkan proyek

---
+++ application/views/auth/erapor.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erapor</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/'); ?>style.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="header-logo">E-LEARNING</div>
+        <div class="header-list">
+            <ul>
+                <li>menu 1</li>
+                <li>menu 2</li>
+                <li>menu 3</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="contents">
+        <div class="contents-item">
+            <img src="assets/img/qr-code.svg">
+            <p></p>
+            <span class="btn message">Lihat Rapor</span>
+        </div>
+        <div class="contents-item">
+            <img src="assets/img/g-meet.svg">
+            <p></p>
+            <span class="btn message">Konsultasi</span>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer-logo">E-LEARNING</div>
+        <div class="footer-list">
+            <ul>
+                <li>menu 1</li>
+                <li>menu 2</li>
+                <li>menu 3</li>
+            </ul>
+        </div>
+    </div>
+</body>
+
+</html>
